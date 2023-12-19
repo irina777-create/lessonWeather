bc552697c1467fa6f55f80fa3ff49eea6d92bb9e
Hryhoreieva Iryna <grigoreva108@gmail.com>
2023-12-19 21:43:18 +0200

    изменения main.js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+//<!--За допомогою ajax-запиту вивести погоду
+//http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19
+//q=XXX - місто, для якого показати погоду
+//temp – температура
+//pressure - тиск
+//description – опис
+//humidity – вологість
+//speed – швидкість вітру
+//deg - напрям у градусах
+//icon - значок, де 10d код іконки
+//http://openweathermap.org/img/w/10d.png
 const weatherBlock=document.querySelector('#weather');
 const server='http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19';
-const responce=await fetch(server,{
+const responce= fetch(server,{
     method:'Get',
 })
 const responceResult =await response.json();
@@ -19,18 +30,27 @@ function getWeather(data) {
     const weatherIcon = data.weather[0].icon;
 
 
-    const template =
-        '<div class="weather_header"
-        '    <div class="weather_main"
-        '        <div class="weather_city">${location}</div>
-        '    <div class="weather_status">${weaterStatus}</div>
-        '</div>
-        '    <div class="weather_icon">
-        '        <img src="http://openweathermap.org/img/w/10d.png"alt="Clouds">
-        '    </div>
-        '</div>
-        '    <div class="weather_temp">${temp}</div>
-        '    <div class="weather_feels:13"$</div>'
+    const template =` '< class="weather_header"
+        <div class="weather_main"
+           <div class="weather_city">${q}</div>
+       <div class="weather_status">${temp}</div>
+    </div> 
+    <div class="weather_status">${pressure}
+    </div>
+     <div class="weather_status">${description}
+    </div>
+    </div> <div class="weather_status">${speed}
+    </div>
+    </div> <div class="weather_status">${dec}
+    </div>
+    </div>
+    <div class="weather_icon">
+        <img src="http://openweathermap.org/img/w/10d.png"alt="Clouds">    </div>
+    </div>
+
+ </div>
+}`
+
 }
 if (weatherBlock){
     loadWeather();
